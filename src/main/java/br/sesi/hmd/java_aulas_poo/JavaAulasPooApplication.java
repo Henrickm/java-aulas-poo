5edc4acde37bace9c2b05a6736c99d5fe53e0cc1
Henrick <escola.sesi016@gmail.com>
2024-09-06 09:46:16 -0300

    primeiro commit

---
+++ src/main/java/br/sesi/hmd/java_aulas_poo/JavaAulasPooApplication.java
@@ -0,0 +1,14 @@
+package br.sesi.hmd.java_aulas_poo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class
+JavaAulasPooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaAulasPooApplication.class, args);
+	}
+
+}
---
+++ src/main/java/br/sesi/hmd/modelo/Pessoa.java
@@ -0,0 +1,8 @@
+package br.sesi.hmd.modelo;
+
+public class Pessoa {
+    float peso;
+    float altura;
+    int idade;
+    String nome;
+}
